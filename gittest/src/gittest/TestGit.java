6eba5b04d66667cba602cb3d05886a7518080471
pascal1062 <pascal1062@gmail.com>
2015-01-04 14:18:27 -0500

    commentaire sur la 2e console

--- gittest/src/gittest/TestGit.java
+++ gittest/src/gittest/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bonjour à vous maintenant");
-		System.out.println("Bonjour encore!");
+		//System.out.println("Bonjour encore!");
 	}
 
 }
